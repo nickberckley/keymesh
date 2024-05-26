a220ea91b8948652c2512a3858cfd507ee33c078
Nika Kutsniashvili <nickberckley@gmail.com>
2024-05-27 03:44:00 +0400

    Versioning for property updates and animation transfer

--- versioning.py
+++ versioning.py
@@ -7,24 +7,78 @@ from .functions.poll import obj_data_type
 @bpy.app.handlers.persistent
 def populate_keymesh_blocks(scene):
     for obj in bpy.data.objects:
-        # is_not_keymesh_object
-        if obj.keymesh.get("Data") is None:
+        # is_not_legacy_keymesh_object
+        if obj.get("Keymesh ID") is None:
             continue
-        obj_keymesh_id = obj.keymesh.get("ID")
+
+        # Convert Object Properties
+        obj_legacy_keymesh_id = obj.get("Keymesh ID", None)
+        obj_legacy_keymesh_data = obj.get("Keymesh Data", None)
+        obj.keymesh["ID"] = obj_legacy_keymesh_id
+        obj.keymesh["Keymesh Data"] = obj_legacy_keymesh_data
+        del obj["Keymesh ID"]
+        del obj["Keymesh Data"]
+        if obj.get("Keymesh Name") is not None:
+            del obj["Keymesh Name"]
 
         # list_objects_blocks
         unregistered_blocks = []
         for block in obj_data_type(obj):
-            if block.keymesh.get("ID") is None:
+            if block.get("Keymesh ID") is None:
                 continue
 
-            if block.keymesh.get("ID") == obj_keymesh_id:
+            if block.get("Keymesh ID") == obj_legacy_keymesh_id:
                 unregistered_blocks.append(block)
 
-        # register_to_object
+                # Convert Data Properties
+                block_legacy_keymesh_id = block.get("Keymesh ID", None)
+                block_legacy_keymesh_data = block.get("Keymesh Data", None)
+                block.keymesh["ID"] = block_legacy_keymesh_id
+                block.keymesh["Data"] = block_legacy_keymesh_data
+                del block["Keymesh ID"]
+                del block["Keymesh Data"]
+                if block.get("Keymesh Name") is not None:
+                    del block["Keymesh Name"]
+
+
+        # Register to Object
         for block in unregistered_blocks:
             block_registry = obj.keymesh.blocks.add()
             block_registry.block = block
+
+
+        # Transfer Animation
+        anim_data = obj.animation_data
+        if anim_data:
+            for fcurve in anim_data.action.fcurves:
+                if fcurve.data_path == '["Keymesh Data"]':
+                    original_fcurve = fcurve
+                    for keyframe in fcurve.keyframe_points:
+                        frame = int(keyframe.co[0])
+                        value = keyframe.co[1]
+                        
+                        obj.keymesh["Keymesh Data"] = value
+                        obj.keyframe_insert(data_path='keymesh["Keymesh Data"]', frame=frame)
+
+            # transfer_keyframe_values
+            for fcurve in anim_data.action.fcurves:
+                if fcurve.data_path == 'keymesh["Keymesh Data"]':
+                    for keyframe in fcurve.keyframe_points:
+                        frame = int(keyframe.co[0])
+                        for orig_keyframe in original_fcurve.keyframe_points:
+                            if int(orig_keyframe.co[0]) == frame:
+                                keyframe.interpolation = orig_keyframe.interpolation
+                                keyframe.easing = orig_keyframe.easing
+                                keyframe.handle_left_type = orig_keyframe.handle_left_type
+                                keyframe.handle_right_type = orig_keyframe.handle_right_type
+                                keyframe.handle_left = orig_keyframe.handle_left
+                                keyframe.handle_right = orig_keyframe.handle_right
+                                break
+
+            # remove_legacy_fcurve
+            for fcurve in anim_data.action.fcurves:
+                if fcurve.data_path == '["Keymesh Data"]':
+                    obj.animation_data.action.fcurves.remove(fcurve)
 
 
 
