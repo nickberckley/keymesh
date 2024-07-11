e1e3a2407cfcbc0bf3a47254dd5fdd3324e8f628
Nika Kutsniashvili <nickberckley@gmail.com>
2024-07-11 16:45:54 +0400

    Show keymaps in preferences
    Implementation details:
    - Using custom `draw_kmi` function in order to remove redundant "Keymesh" from names and instead include `path` property, otherwise names are not helpful. Also allows to simplify it and make UI little bit prettier.
    - `get_hotkey_entry_item` currently allows specifying only one property. In future if I implement shortcuts that depend on more than one property specified it has to be refactored.

--- preferences.py
+++ preferences.py
@@ -20,10 +20,104 @@ def update_sidebar_category(self, context):
         bpy.utils.register_class(cls)
 
 
+# Keymaps
+def get_hotkey_entry_item(km, kmi_name, kmi_prop=None, kmi_value=None):
+    """Returns keymap with given operator name and `path` property value"""
+    for i, km_item in enumerate(km.keymap_items):
+        if km.keymap_items.keys()[i] == kmi_name:
+            if kmi_prop:
+                if getattr(km.keymap_items[i].properties, kmi_prop) == kmi_value:
+                    return km_item
+            else:
+                return km_item
+
+    return None
+
+
+def draw_kmi(kmi, layout):
+    map_type = kmi.map_type
+
+    col = layout.column()
+    if kmi.show_expanded:
+        col = col.column()
+        box = col.box()
+    else:
+        box = col.column()
+    box.use_property_split = False
+    split = box.split(align=True)
+
+    # Header
+    row = split.row(align=True)
+    row.prop(kmi, "show_expanded", text="", emboss=False)
+    row.prop(kmi, "active", text="", emboss=False)
+
+    name = kmi.name.replace(" Keymesh", "")
+    row.label(text=name + " (" + kmi.properties.path.capitalize() + ")")
+
+    row = split.row()
+    row.prop(kmi, "map_type", text="")
+    if map_type == 'KEYBOARD':
+        row.prop(kmi, "type", text="", full_event=True)
+    elif map_type == 'MOUSE':
+        row.prop(kmi, "type", text="", full_event=True)
+    elif map_type == 'NDOF':
+        row.prop(kmi, "type", text="", full_event=True)
+    elif map_type == 'TWEAK':
+        subrow = row.row()
+        subrow.prop(kmi, "type", text="")
+        subrow.prop(kmi, "value", text="")
+    elif map_type == 'TIMER':
+        row.prop(kmi, "type", text="")
+    else:
+        row.label()
+
+    # NOTE: there is a bug that always shows next icon in list instead of defined,
+    # NOTE: so 'REMOVE' is used instead of 'PANEL_CLOSE'
+    row.prop(kmi, "active", text="", icon='REMOVE' if kmi.active else 'TRACKING_CLEAR_BACKWARDS', emboss=False)
+
+
+    # Body
+    if kmi.show_expanded:
+        split = box.split(factor=0.5)
+        split.prop(kmi, "idname", text="")
+
+        if map_type not in {'TEXTINPUT', 'TIMER'}:
+            sub = split.column()
+            subrow = sub.row(align=True)
+
+            if map_type == 'KEYBOARD':
+                subrow.prop(kmi, "type", text="", event=True)
+                subrow.prop(kmi, "value", text="")
+                subrow_repeat = subrow.row(align=True)
+                subrow_repeat.prop(kmi, "repeat", text="Repeat")
+                subrow_repeat.active = kmi.value in {'ANY', 'PRESS'}
+
+            elif map_type in {'MOUSE', 'NDOF'}:
+                subrow.prop(kmi, "type", text="")
+                subrow.prop(kmi, "value", text="")
+
+            if map_type in {'KEYBOARD', 'MOUSE'} and kmi.value == 'CLICK_DRAG':
+                subrow = sub.row()
+                subrow.prop(kmi, "direction")
+
+            subrow = sub.row()
+            subrow.scale_x = 0.75
+            subrow.prop(kmi, "any", toggle=True)
+            subrow.prop(kmi, "shift_ui", toggle=True)
+            subrow.prop(kmi, "ctrl_ui", toggle=True)
+            subrow.prop(kmi, "alt_ui", toggle=True)
+            subrow.prop(kmi, "oskey_ui", text="Cmd", toggle=True)
+            subrow.prop(kmi, "key_modifier", text="", event=True)
+
+        # Properties
+        box.template_keymap_item_properties(kmi)
+
+
 
 #### ------------------------------ PREFERENCES ------------------------------ ####
 
 class KeymeshAddonPreferences(bpy.types.AddonPreferences):
+
     bl_idname = __package__
 
     # ui
@@ -117,6 +211,33 @@ class KeymeshAddonPreferences(bpy.types.AddonPreferences):
         col.separator()
         col.prop(self, "versioning")
         col.prop(self, "debug")
+
+
+        # Keymaps
+        layout.separator()
+        box = layout.box()
+        box.label(text="Hotkeys:")
+        kc = bpy.context.window_manager.keyconfigs.user
+
+        # insert_shortcuts
+        kmi = []
+        km = kc.keymaps["3D View"]
+        kmi.append(get_hotkey_entry_item(km, "object.keyframe_object_data", 'path', "FORWARD"))
+        kmi.append(get_hotkey_entry_item(km, "object.keyframe_object_data", 'path', "BACKWARD"))
+        for kmi in kmi:
+            if kmi:
+                box.context_pointer_set("keymap", km)
+                draw_kmi(kmi, box)
+
+        # jump_shortcuts
+        kmi = []
+        km = kc.keymaps["Frames"]
+        kmi.append(get_hotkey_entry_item(km, "timeline.keymesh_frame_jump", 'path', "FORWARD"))
+        kmi.append(get_hotkey_entry_item(km, "timeline.keymesh_frame_jump", 'path', "BACKWARD"))
+        for kmi in kmi:
+            if kmi:
+                box.context_pointer_set("keymap", km)
+                draw_kmi(kmi, box)
 
 
 
