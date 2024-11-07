6e6ccd85f1006b5b783601f8b9dace310c23a3c5
Nika Kutsniashvili <nickberckley@gmail.com>
2024-11-07 22:59:12 +0400

    Remove old implementation code and files

--- operators/generate_thumbnails.py
+++ operators/generate_thumbnails.py
@@ -1,158 +1,7 @@
-import bpy, os, mathutils, subprocess, time
-from ..functions.thumbnail import previews_register, previews_unregister
+import bpy, os, mathutils
 
 
 #### ------------------------------ OPERATORS ------------------------------ ####
-
-# class OBJECT_OT_keymesh_thumbnails_generate(bpy.types.Operator):
-#     bl_idname = 'object.keymesh_thumbnails_generate'
-#     bl_label = 'Render Keymesh Thumbnails'
-#     bl_description = "Batch-generate thumbnails for Keymesh blocks by rendering them in the background"
-#     bl_options = {'INTERNAL'}
-
-#     directory: bpy.props.StringProperty(
-#         name = "Directory",
-#         description = "Directory to store .jpg images for generated thumbnails",
-#         subtype = 'DIR_PATH',
-#     )
-
-#     def draw(self, context):
-#         layout = self.layout
-#         layout.prop(self, "directory", text="")
-#         col = layout.column()
-#         col.label(text="Will create .jpg thumbnails for Keymesh blocks. This may take a few minutes.")
-#         col.label(text="Thumbnails will be rendered with active viewport shading properties", icon='INFO')
-
-
-#     def generate_thumbnail(self, obj, block, viewport):
-#         directory = bpy.path.abspath(self.directory)
-#         thumbnail_path = directory + str(block.name) + ".png"
-
-#         blends_folder = os.path.dirname(os.path.realpath(__file__))
-#         blends_folder = os.path.dirname(blends_folder)
-#         blends_folder = os.path.join(blends_folder, "blends")
-
-#         # start_new_instance_of_blender
-#         cmd = [bpy.app.binary_path]
-#         cmd.append(os.path.join(blends_folder, "thumbnail.blend"))
-#         cmd.append("--background")
-#         cmd.append("--python")
-#         cmd.append(os.path.join(blends_folder, "thumbnail.py"))
-
-#         # Args for Script
-#         cmd.append('--')
-#         cmd.append(bpy.data.filepath)
-#         cmd.append(obj.name)
-#         cmd.append(block.name)
-#         cmd.append('150') # NOTE: thumbnail_image_size
-#         cmd.append(thumbnail_path) # NOTE: output_path
-
-#         # viewport_shading
-#         # cmd.append(viewport.light)
-#         # cmd.append(viewport.selected_studio_light.name)
-#         # cmd.append(str(viewport.studiolight_rotate_z))
-#         # cmd.append(str(viewport.use_world_space_lighting))
-
-#         # cmd.append(viewport.color_type)
-#         # cmd.append(str(viewport.single_color))
-#         # cmd.append(str(viewport.background_type))
-#         # cmd.append(str(viewport.background_color))
-
-#         # cmd.append(str(viewport.show_backface_culling))
-#         # cmd.append(str(viewport.show_xray))
-#         # cmd.append(str(viewport.xray_alpha))
-#         # cmd.append(str(viewport.show_object_outline))
-#         # cmd.append(str(viewport.object_outline_color))
-
-#         # cmd.append(str(viewport.show_shadows))
-#         # cmd.append(str(viewport.shadow_intensity))
-
-#         # cmd.append(str(viewport.show_cavity))
-#         # cmd.append(viewport.cavity_type)
-#         # cmd.append(str(viewport.cavity_ridge_factor))
-#         # cmd.append(str(viewport.cavity_valley_factor))
-#         # cmd.append(str(viewport.curvature_ridge_factor))
-#         # cmd.append(str(viewport.curvature_valley_factor))
-
-#         # cmd.append(str(viewport.use_dof))
-#         # cmd.append(str(viewport.show_specular_highlight))
-
-
-#         # cmd.append(viewport.wireframe_color_type)
-
-#         # print(cmd)
-#         subprocess.run(cmd)
-
-
-#     def execute(self, context):
-#         obj = context.active_object
-#         directory = bpy.path.abspath(self.directory)
-#         viewport = bpy.context.space_data.shading
-
-#         # TODO: Abort if no directory
-#         # TODO: Abort if file not saved
-#         # TODO: Stamp block name in the image
-#         # TODO: Test with multiple Keymesh objects in scene
-#         # TODO: Poll to only execute in 3D viewport
-#         # TODO: Send in shadow and cavity props
-
-#         # list_keymesh_blocks_(without_thumbnails)
-#         blocks = []
-#         for block in obj.keymesh.blocks:
-#             # if block.thumbnail != "":
-#             #     continue
-
-#             # image_path = os.path.join(directory, block.name + ".png")
-#             # if os.path.isfile(image_path):
-#             #     continue
-
-#             blocks.append(block.block)
-
-#         if not blocks:
-#             self.report({'INFO'}, "All Keymesh blocks already have thumbnails")
-#             return {'FINISHED'}
-
-#         # Refresh Thumbnails
-#         previews_unregister()
-#         previews_register()
-
-#         threaded = True  # NOTE: Set to False for debugging.
-#         errors = []
-#         # if threaded:
-#         from concurrent.futures import ThreadPoolExecutor
-#         executor = ThreadPoolExecutor(max_workers=8)
-#         threads = []
-#         for block in blocks:
-#             t = executor.submit(self.generate_thumbnail, obj, block, viewport)
-#             threads.append(t)
-
-#         while (any(t._state != "FINISHED" for t in threads)):
-#             num_finished = 0
-#             for t in threads:
-#                 if t._state == "FINISHED":
-#                     num_finished += 1
-#                     if t.result() is not None:
-#                         errors.append(t.result())
-#             time.sleep(2)
-#         # else:
-#         #     for num_finished, block in enumerate(blocks):
-#         #         self.generate_thumb(block)
-
-#         if errors:
-#             for error in errors:
-#                 print(error)
-
-#         # Assign Thumbnails
-#         for block in obj.keymesh.blocks:
-#             image_path = os.path.join(directory, block.name + ".png")
-#             if os.path.isfile(image_path):
-#                 block.thumbnail = image_path
-
-#         return {'FINISHED'}
-    
-#     def invoke(self, context, event):
-#         return context.window_manager.invoke_props_dialog(self)
-
 
 class OBJECT_OT_keymesh_thumbnails_generate(bpy.types.Operator):
     bl_idname = 'object.keymesh_thumbnails_generate'
@@ -229,9 +78,9 @@ class OBJECT_OT_keymesh_thumbnails_generate(bpy.types.Operator):
         if directory == "":
             self.report({'ERROR'}, "Filepath wasn't provided")
             return {'CANCELLED'}
-        if not os.access(directory, os.W_OK):
-            self.report({'ERROR'}, "Thumbnails couldn't be generated. Output path is incorrect or needs permission")
-            return {'CANCELLED'}
+        # if not os.access(directory, os.W_OK):
+        #     self.report({'ERROR'}, "Thumbnails couldn't be generated. Output path is incorrect or needs permission")
+        #     return {'CANCELLED'}
 
         # list_keymesh_blocks
         if self.selection == 'ALL':
